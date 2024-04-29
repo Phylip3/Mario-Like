f5eefeecf920837762a57f988f72b42c5479765a
KamelAkar <76403237+Siillvercrow@users.noreply.github.com>
2024-04-29 14:01:33 +0200

    Center texte Menu

--- Pong/src/CoreGame/Menu.cpp
+++ Pong/src/CoreGame/Menu.cpp
@@ -13,23 +13,41 @@ Menu::Menu(sf::RenderWindow& win) : window(win), enteringText(false), selectedIt
     titles[0] = "Nouveau serveur";
     titles[1] = "Rejoindre une partie";
 
-    for (int i = 0; i < 2; i++) {
+    int numberOfMenuItems = 2;  
+    int spacing = 100; 
+    
+    float totalWidth = 0;
+    std::vector<sf::FloatRect> textRects(numberOfMenuItems);
+    for (int i = 0; i < numberOfMenuItems; i++) {
         menuItems[i].setFont(font);
         menuItems[i].setString(titles[i]);
         menuItems[i].setCharacterSize(24);
         menuItems[i].setFillColor(sf::Color::White);
-        menuItems[i].setPosition(300, 200 + i * 100);
+
+        textRects[i] = menuItems[i].getLocalBounds();
+        totalWidth += textRects[i].width;
     }
+
+    // Ajouter les espaces entre les textes au totalWidth
+    totalWidth += (numberOfMenuItems - 1) * spacing;
+
+    // Positionner le premier élément de texte
+    float currentX = (window.getSize().x - totalWidth) / 2.0f;  // Début du premier texte
+    for (int i = 0; i < numberOfMenuItems; i++) {
+        menuItems[i].setOrigin(textRects[i].left + textRects[i].width / 2.0f, textRects[i].top + textRects[i].height / 2.0f);
+        menuItems[i].setPosition(sf::Vector2f(currentX + textRects[i].width / 2.0f, window.getSize().y / 2.0f));
+
+        // Mise à jour de currentX pour le prochain élément de texte
+        currentX += textRects[i].width + spacing;
+    }
+
 
     inputText.setFont(font);
     inputText.setCharacterSize(24);
     inputText.setFillColor(sf::Color::White);
     inputText.setPosition(window.getSize().x / 2, window.getSize().y / 2);
 
-    serverStatus.setFont(font);
-    serverStatus.setCharacterSize(24);
-    serverStatus.setFillColor(sf::Color::Green);
-    serverStatus.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+    
 }
 
 void Menu::handleEvent(sf::Event& event) {
@@ -43,6 +61,14 @@ void Menu::handleEvent(sf::Event& event) {
                     serverRunning = true;
                     serverThread = std::thread(startServer);
                     serverStatus.setString("Serveur lancé sur le port 2003!");
+
+                    serverStatus.setFont(font);
+                    serverStatus.setCharacterSize(24);
+                    serverStatus.setFillColor(sf::Color::Green);
+                    sf::FloatRect textRect = serverStatus.getLocalBounds();
+                    serverStatus.setOrigin(textRect.width / 2, textRect.height / 2);
+                    serverStatus.setOrigin(serverStatus.getLocalBounds().width / 2, serverStatus.getLocalBounds().height / 2);
+                    serverStatus.setPosition(window.getSize().x / 2, window.getSize().y / 2);
                 }
                 else if (titles[i] == "Rejoindre une partie") {
                     connectToServer();
